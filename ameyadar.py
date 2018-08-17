32d92d43c5c4dfb028c8e48cd7e4fee3e04acf3c
kkiyama117 <k.kiyama117@gmail.com>
2018-08-18 07:45:47 +0900

    First Commit by kkiyama117
    Refactoring
    - Add .gitignore
    - Fix Bug (Undefined variables)
    - Make Env File

--- ameyadar.py
+++ ameyadar.py
@@ -1,5 +1,5 @@
 # coding: UTF-8
-'''
+"""
 MIT License
 
 Copyright (c) 2018 shyguy
@@ -21,38 +21,49 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+"""
 
-import json, os, time, math, urllib.request
+# Libs
+# main
+import json, os, math, urllib.request
 from requests_oauthlib import OAuth1Session
-import datetime as dt
 from scipy.stats import norm
+# devide config
+from decouple import config
 
 # Twitter API
-twitter = OAuth1Session(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"], os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
+
+twitter = OAuth1Session(config("CONSUMER_KEY"), config("CONSUMER_SECRET"),
+                        config("ACCESS_TOKEN"),
+                        config("ACCESS_TOKEN_SECRET"))
 
 # Yahoo API
-YAHOO_APP_ID = os.environ["YAHOO_APP_ID"]
+YAHOO_APP_ID = config("YAHOO_APP_ID")
 
-COORDINATES = '135.7849,35.02799' # longitude and latitude
+COORDINATES = '135.7849,35.02799'  # longitude and latitude of KU
 rainEmoji = ['🌂', '🌦', '☂️', '🌧', '☔', '⛈', '🌀']
 
-rain_url = 'https://map.yahooapis.jp/weather/V1/place?coordinates=%s&appid=%s&output=json&interval=10'%(COORDINATES,YAHOO_APP_ID)
-response = urllib.request.urlopen(rain_url)
-content = json.loads(response.read().decode('utf-8'))
+RAIN_URL_BASE = 'https://map.yahooapis.jp/weather/V1/place?coordinates=%s' \
+                '&appid=%s&output=json&interval=10'
+rain_url = RAIN_URL_BASE % (COORDINATES, YAHOO_APP_ID)
+content = json.loads(urllib.request.urlopen(rain_url).read().decode('utf-8'))
 rainfall = 0
 emoji = ''
 for var in range(7):
-    x = norm.pdf(var, 0, 5)/norm.pdf(0, 0, 5)
-    y = content['Feature'][0]['Property']['WeatherList']['Weather'][var]['Rainfall']
-    rainfall += x*y
+    x = norm.pdf(var, 0, 5) / norm.pdf(0, 0, 5)
+    y = content['Feature'][0]['Property']['WeatherList']['Weather'][var][
+        'Rainfall']
+    rainfall += x * y
 if rainfall != 0:
-    cubed = min(math.ceil(math.log(math.ceil(rainfall),3)), 6)
-    emoji = rainEmoji[cubed]
-req0 = twitter.get('https://api.twitter.com/1.1/account/verify_credentials.json')
+    cubed = min(math.ceil(math.log(math.ceil(rainfall), 3)), 6)
+    emoji = rainEmoji[int(cubed)]
+req0 = twitter.get(
+    'https://api.twitter.com/1.1/account/verify_credentials.json')
 oldName = json.loads(req0.text)['name']
 for num in range(7):
-    oldName = oldName.replace(rainEmoji[num],"")
-newName = oldName.replace(defaultEmoji,"") + emoji
-reqPost1 = twitter.post('https://api.twitter.com/1.1/account/update_profile.json?name=%s'%newName)
-print("new name: "+newName)
+    oldName = oldName.replace(rainEmoji[num], "")
+newName = oldName + emoji
+# reqPost1 = twitter.post(
+#     'https://api.twitter.com/1.1/account/update_profile.json?name=%s'
+#     % newName)
+print("new name: " + newName)
